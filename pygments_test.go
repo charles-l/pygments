84c0ea68be5f90b3f712e184b4bc4943dd6c2ac1
nc <charles.saternos@gmail.com>
2017-01-14 12:54:17 -0500

    added line numbers

--- pygments_test.go
+++ pygments_test.go
@@ -18,7 +18,7 @@ func TestWhich(t *testing.T) {
 
 func TestHighlighting(t *testing.T) {
 	v, _ := Highlight([]byte("print \"Hello World!\""), "python", "html", "utf-8")
-	e := "<div class=\"highlight\"><pre><span></span><span class=\"k\">print</span> <span class=\"s2\">&quot;Hello World!&quot;</span>\n</pre></div>\n"
+	e := "<table class=\"highlighttable\"><tr><td class=\"linenos\"><div class=\"linenodiv\"><pre>1</pre></div></td><td class=\"code\"><div class=\"highlight\"><pre><span></span><span class=\"k\">print</span> <span class=\"s2\">&quot;Hello World!&quot;</span>\n</pre></div>\n</td></tr></table>"
 	if v != e {
 		t.Error("Expected", e, ", got", v)
 	}
